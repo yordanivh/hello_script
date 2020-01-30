c41e7e663df30c217e2bc8677acddbf823bb4a36
YordanIH <yordan@hashicorp.com>
2020-01-30 14:06:00 +0200

    Commit

---
+++ test_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+out = $(./script.sh)
+
+if [ "${out}" = "Hello world"]; then
+	echo "Everything is OK"
+	exit 0
+else
+	echo "Failure"
+	echo "Expected output was Hello world"
+        echo "Instead recieve ${out}"
+	exit 1
+fi	
